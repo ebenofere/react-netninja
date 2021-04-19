0cdc5c4e22d7673cb886f0834f06cb954d21e9a1
Eben Ofere <eofere@gmail.com>
2021-04-19 12:08:52 +0100

    Learning to use "props"

--- src/Home.js
+++ src/Home.js
@@ -1,25 +1,16 @@
 import { useState } from 'react';
+import BlogList from './BlogList';
 
 const Home = () => {
-    // let name = 'Mario';
     const [blogs, setBlogs] = useState([
         { id:1, title: 'My new website', author: 'mario', body: 'lorem ipsum...' },
         { id:2, title: 'Welcome to the party', author: 'eben', body: 'lorem ipsum...' },
         { id:3, title: 'Web dev top tips', author: 'yoshi', body: 'lorem ipsum...' }
     ]);
-
-
+ 
     return (
         <div className="home">
-            {blogs.map((blog) => (
-                <div className="blog-preview" key={blog.id}>
-                    <h2>{ blog.title }</h2>
-                    <p>Written by { blog.author }</p>
-
-                </div>
-            ))
-            }
-        
+            <BlogList blogs={blogs} title="All Blogs!" />
         </div>
     );
 }
