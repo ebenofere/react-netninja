328190e4a92d0b1810939abb4643665297f708b5
Eben Ofere <eofere@gmail.com>
2021-04-19 12:21:02 +0100

    Reusing Components

--- src/Home.js
+++ src/Home.js
@@ -5,12 +5,13 @@ const Home = () => {
     const [blogs, setBlogs] = useState([
         { id:1, title: 'My new website', author: 'mario', body: 'lorem ipsum...' },
         { id:2, title: 'Welcome to the party', author: 'eben', body: 'lorem ipsum...' },
-        { id:3, title: 'Web dev top tips', author: 'yoshi', body: 'lorem ipsum...' }
+        { id:3, title: 'Web dev top tips', author: 'mario', body: 'lorem ipsum...' }
     ]);
  
     return (
         <div className="home">
             <BlogList blogs={blogs} title="All Blogs!" />
+            <BlogList blogs={blogs.filter((blog) => blog.author === 'mario' )} title="Mario's Blogs!" />
         </div>
     );
 }
