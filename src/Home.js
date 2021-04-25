bf63fa6bc3604a1b40cc5f0e3d518e5302301912
Eben Ofere <eofere@gmail.com>
2021-04-25 02:08:03 +0100

    The React Router

--- src/Home.js
+++ src/Home.js
@@ -5,7 +5,7 @@ import useFetch from './useFetch';
 const Home = () => {
     const { data: blogs, isPending, error } = useFetch('http://localhost:8000/blogs');
     
-        return (
+    return (
         <div className="home">
         { error && <div>{ error }</div> }
         { isPending && <div>Loading...</div> }
